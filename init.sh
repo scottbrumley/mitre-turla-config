cddec34ec87b12d2db63ac73849fe85baa436d79
Ubuntu <adminuser@brumnet-palo--support-dns-srv1.b3xs02a0isbetpxkdk5xzqmq4f.bx.internal.cloudapp.net>
2024-03-31 12:16:11 +0000

    Creating an initialization script to automate as much of the first steps as possible.

---
+++ init.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+ANSIBLE_HOST_KEY_CHECKING=False
+printenv ANSIBLE_HOST_KEY_CHECKING
+
+sudo apt update
+sudo apt install -y ansible sshpass
+/usr/bin/ansible-galaxy collection install chocolatey.chocolatey
+/usr/bin/ansible-galaxy collection install ansible.windows
+
+git clone https://github.com/center-for-threat-informed-defense/adversary_emulation_library.git ../adversary_emulation_library
+
+echo " "
+echo "To test Linux servers try this command: ansible lin -i inventory -m ping --ask-pass -u adminuser"
+echo " "
+echo "To test Windows server try this command: ansible win -i inventory -m win_ping --ask-pass -u adminuser"
+echo "If the environment was just built then login into each windows asset to kick of the first build script.  Then run this again"
+echo " "
+echo "To build the Support Network run: ansible-playbook -i inventory support.yml --ask-pass -u adminuser"
