2942a8afa5b2ba9189befcec11205292da7cb377
Ubuntu <adminuser@brumnet-palo--support-dns-srv1.2xf4r2tprlcurgqbxqbzrgivnh.bx.internal.cloudapp.net>
2024-04-23 12:36:16 +0000

    Update ansible.cfg to set time per task

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
+echo "#### Update APT ####"
 sudo apt update
+echo "#### Install Ansible and Collections ####"
 sudo apt install -y ansible sshpass
 ansible-galaxy collection install ansible.windows
 ansible-galaxy collection install community.windows
@@ -8,6 +10,10 @@ ansible-galaxy collection install microsoft.ad
 ansible-galaxy collection install chocolatey.chocolatey
 ansible-galaxy collection install community.general
 
+echo "#### Configure Timestamps for Ansible ####"
+sudo sed -i 's/# some basic default values.../callback_whitelist = profile_tasks/' /etc/ansible/ansible.cfg
+
+echo "#### Clone Adversary Emulation Library from GitHub ####"
 git clone https://github.com/center-for-threat-informed-defense/adversary_emulation_library.git ../adversary_emulation_library
 
 echo " "
