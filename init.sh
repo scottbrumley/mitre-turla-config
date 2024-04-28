feaf60e547215cd0f35819006f18047ff99297fb
Ubuntu <adminuser@brumnet-palo--support-dns-srv1.ybwby0q0tvlurgpfzj0pd32mjc.bx.internal.cloudapp.net>
2024-04-28 11:13:53 +0000

    Updating instructions in init.sh

--- init.sh
+++ init.sh
@@ -15,11 +15,6 @@ sudo sed -i 's/# some basic default values.../callback_whitelist = profile_tasks
 
 echo "#### Clone Adversary Emulation Library from GitHub ####"
 git clone https://github.com/center-for-threat-informed-defense/adversary_emulation_library.git ../adversary_emulation_library
-
 echo " "
-echo "To test Linux servers try this command: ansible lin -i inventory -m ping --ask-pass -u adminuser"
-echo " "
-echo "To test Windows server try this command: ansible win -i inventory -m win_ping --ask-pass -u adminuser"
-echo "If the environment was just built then login into each windows asset to kick of the first build script.  Then run this again"
-echo " "
-echo "To build the Support Network run: ansible-playbook -i inventory support.yml --ask-pass -u adminuser"
+echo "To build the whole environment run:"
+echo "ansible-playbook -i inventory runall.yml --ask-pass -u adminuser"
